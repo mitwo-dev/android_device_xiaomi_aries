f55c3b9038edfcc58d4a583c1e44670192066dc6
M1cha <sigmaepsilon92@gmail.com>
2015-05-31 20:27:54 +0200

    AndroidBoard: move kernel to kernel/xiaomi/msm8960 and fix LOCAL_PATH bug
    Change-Id: I4779d121c31b16a74137fcd4c5a638f8dba07b0d

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,12 +1,12 @@
-LOCAL_PATH := $(cal my-dir)
+LOCAL_PATH := $(call my-dir)
 #-------------------------------------
 # linux kernel compile
 # -----------------------------------
 #
-TARGET_KERNEL_SOURCE := kernel/msm8960
+TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8960
 ifeq ($(KERNEL_DEFCONFIG),)
     KERNEL_DEFCONFIG := aries-perf-user_defconfig
-    include kernel/msm8960/AndroidKernel.mk
+    include $(TARGET_KERNEL_SOURCE)/AndroidKernel.mk
 $(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP) $(TARGET_PREBUILT_KERNEL_INCLUDE)
 	$(transform-prebuilt-to-target)
 endif
