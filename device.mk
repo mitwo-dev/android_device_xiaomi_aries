1dc52fd5cd20da0c81860d8485dd78c4e7b20e57
M1cha <sigmaepsilon92@gmail.com>
2015-06-02 16:31:50 +0200

    Switch to wcnss_service to initialize wifi
    qcom's way to set the mac from userspace is much better.
    
    Change-Id: I2099af79faa57e06dd42a10d7ef0cf0850e3c453

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 
 # Wi-Fi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/rootdir/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini\
 
 # Audio
 PRODUCT_COPY_FILES += \
